065f6e4aad8114e8c2b43ce66acbac7e02cc6eee
Andrii Maksymonko <max.maxvell1@gmail.com>
2023-11-09 18:20:05 +0200

    add kbot v1.0.2

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -14,15 +14,16 @@ import (
 	telebot "gopkg.in/telebot.v3"
 )
 
-var {
+var (
 	//TeleToken bot
 	TeleToken = os.Getenv("TELE_TOKEN")
-}
+)
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -32,17 +33,35 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		fmt.Printf("kbot %s started", appVersion)
-		kbot, err := telebot.NewBot{telebot.settings{
-			URL:	"",
-			Token:	TeleToken,
+
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
 			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
-		}} 
+		})
 
 		if err != nil {
 			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 			return
 		}
 
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s", appVersion))
+
+				return err
+			}
+
+			return err
+
+		})
+		kbot.Start()
 	},
 }
 
